a5fa5bef8311e4dde8b511f79f95184980001a99
Wayne <waynens@miamioh.edu>
2018-04-25 10:34:41 -0400

    seting up repository, going t ointegrate html pages

---
+++ Clothes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clothes.Startup))]
+namespace Clothes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
